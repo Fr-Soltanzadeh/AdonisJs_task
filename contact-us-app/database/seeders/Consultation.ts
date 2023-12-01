eff236607f9eaba9fed6663b4cb39d85ba0ef09c
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-12-01 16:11:31 +0330

    refactor

--- contact-us-app/database/seeders/Consultation.ts
+++ contact-us-app/database/seeders/Consultation.ts
@@ -1,5 +1,5 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import { ConsultationFactory } from 'Database/factories/index';
+import { ConsultationFactory } from 'Database/factories/Consultation';
 
 export default class extends BaseSeeder {
     public async run () {
